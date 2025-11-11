dce994bcb0c9bfe03f1bcbec25ba5bbfa5593d3a
ewerton.silva <ewerton.silva@dcx.ufpb.br>
2025-11-11 09:38:51 -0300

    Adicionando nova classe Java

---
+++ src/main/java/br/ufpb/dcx/silva/ewerton/Atividade2.java
@@ -0,0 +1,10 @@
+package br.ufpb.dcx.silva.ewerton;
+
+public class Atividade2 {
+        void main () {
+            IO.println("Oi");
+            String nome = IO.readln("Digite seu nome: ");
+            IO.println("Olá" + nome);
+        }
+}
+
